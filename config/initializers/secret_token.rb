29ef1c284d638ca4063b6ebde7d960655d071a4f
Tony <boo@me.com>
2011-08-09 13:29:23 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lizzie1109::Application.config.secret_token = '020e848acb9ec6d234eae1f90b079f7fc504949c06fb8ed524d34c6266a620dc5804f18e9875fb81ef00f5c43a4d6507da27c68c309f17c6797e459b46c5eca3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lizzie1109::Application.config.session_store :cookie_store, :key => '_lizzie11.09_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lizzie1109::Application.config.session_store :active_record_store
